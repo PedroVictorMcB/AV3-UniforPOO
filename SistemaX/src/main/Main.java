49f66ac1943b5a1b5c6a5176bd45685b71236dce
PedroVictorMcB <pedrovictormcb80@gmail.com>
2023-11-15 19:33:02 -0300

    Comecei a criar o diagrama de classes para o sistema bancário

--- SistemaX/src/main/Main.java
+++ SistemaX/src/main/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		String nome1 = "Eduardo Melo";
-		String nome2 = "Bruna Alencar";
 		
-		System.out.print("Aqui comeca nossa jornada contra o tempo! SEJAM BEM VINDOS " + nome1 + " e " + nome2);
+		System.out.print("Aqui comeca nossa jornada contra o tempo! SEJA BEM VINDO " + nome1 + "!!");
 
 	}
 
