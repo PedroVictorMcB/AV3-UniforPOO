4155f0c3a949b087c623ac254e3c5a1126da4426
PedroVictorMcB <pedrovictormcb80@gmail.com>
2023-11-17 20:41:53 -0300

    Iniciei a criação da Classe de controle, TransacaoController.java

---
+++ SistemaBancario/src/controller/TransacaoController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.util.List;
+
+import Model.Transacao;
+
+public class TransacaoController {
+
+	private List <Transacao> transacoes;
+
+	public TransacaoController(List<Transacao> transacoes) {
+		super();
+		this.transacoes = transacoes;
+	}
+
+	public List<Transacao> getTransacoes() {
+		return transacoes;
+	};
+
+	public void setTransacoes(List<Transacao> transacoes) {
+		this.transacoes = transacoes;
+	};
+	
+	public void addTransacao(Transacao transacoes) {
+		
+	};
+	
+	public void removeTransacao(Transacao transacao) {
+		
+	};
+}
