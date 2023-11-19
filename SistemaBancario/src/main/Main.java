05b9f646e823407d5c1d2b01eee38f457c88464e
PedroVictorMcB <pedrovictormcb80@gmail.com>
2023-11-19 10:14:51 -0300

    add sqlite connection

--- SistemaBancario/src/main/Main.java
+++ SistemaBancario/src/main/Main.java
@@ -6,8 +6,14 @@ import Model.DBService;
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		DBService db = new DBService();
-		ContaCorrente contaCorrente = db.queryContaCorrente(1);
+		ContaCorrente contaCorrente = db.queryContaCorrente(3);
 		
 		System.out.println("Seu saldo é " + contaCorrente.getSaldo() + " e seu id é " + contaCorrente.getId());
 
