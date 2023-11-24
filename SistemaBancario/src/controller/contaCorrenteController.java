a50878978e310fcb649a43159ff9abef0d057cfe
PedroVictorMcB <pedrovictormcb80@gmail.com>
2023-11-24 18:32:47 -0300

    Trabalhei no construtor de contaCorrenteController.java

--- SistemaBancario/src/controller/contaCorrenteController.java
+++ SistemaBancario/src/controller/contaCorrenteController.java
@@ -2,29 +2,32 @@ package controller;
 
 import java.util.Date;
 import java.util.List;
-
 import Model.ContaCorrente;
+import Model.EntidadeRelacional;
 import Model.IContaCorrente;
 import Model.Transacao;
+import Model.DBService;
 
-public class contaCorrenteController implements IContaCorrente {
+public class contaCorrenteController extends EntidadeRelacional implements IContaCorrente {
 	
+	private DBService db = new DBService();
 	private ContaCorrente contaCorrente;
+	//ContaCorrente contaCorrente = db.queryContaCorrente(1);
 	
-	public contaCorrenteController(ContaCorrente contaCorrente) {
-		this.contaCorrente = contaCorrente;
+	public contaCorrenteController(int id, DBService db) {
+		super(id);
+		this.db = db;
+		this.contaCorrente = db.queryContaCorrente(id);
 	}
 
 	@Override
-	public float getSaldo(ContaCorrente contaCorrente) {
-		// TODO Auto-generated method stub
-		return saldo;
+	public float getSaldo() {
+		return this.contaCorrente.getSaldo();
 	}
 
 	@Override
 	public void setSaldo(float saldo) {
-		// TODO Auto-generated method stub
-		
+		this.contaCorrente.setSaldo(saldo);
 	}
 	
 	public List<Transacao> extrato(ContaCorrente contaCorrente){
